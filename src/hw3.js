c1fd7bb54f8b21f60bcc4308eca86c8ef4a0c1c6
SPellegrene <pellegrenesam@gmail.com>
2016-10-26 16:04:03 -0500

    Update hw3.js

--- src/hw3.js
+++ src/hw3.js
@@ -3,3 +3,5 @@ var myVarTwo = [10, 20, 30,40][3, 2, 1]
 
    console.assert(myVarOne === 40)
    console.assert(myVarTwo === 20)
+
+// It passes because it reads the last value of myVarOne and the first value of myVarTwo.
